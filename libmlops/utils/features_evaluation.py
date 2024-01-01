a3036046aa605f883e89e87b9944043cadbda1cb
wolffy-au <wolffborg1@gmail.com>
2024-01-01 15:50:22 +1100

    Fixed keep_features to accept feature names

--- libmlops/utils/features_evaluation.py
+++ libmlops/utils/features_evaluation.py
@@ -1,7 +1,13 @@
 # Load libraries
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, RandomForestRegressor, HistGradientBoostingClassifier
+from typing import List, Union
+from sklearn.ensemble import (
+    ExtraTreesClassifier,
+    RandomForestClassifier,
+    RandomForestRegressor,
+    HistGradientBoostingClassifier,
+)
 from sklearn.feature_selection import SelectKBest, f_classif, RFE
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.tree import DecisionTreeRegressor
@@ -10,46 +16,21 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
 
-# Spot Check Feature Selection algorithms
-# models = [
-#     ('ExtraTreesClassifier', ExtraTreesClassifier()),
-#     ('HistGradientBoostingClassifier', HistGradientBoostingClassifier()),
-#     ('RandomForestClassifier', RandomForestClassifier()),
-#     ('RFE', RFE(LogisticRegression(solver='lbfgs', max_iter=1000))),
-#     ('LinearRegression', LinearRegression()),
-#     ('DecisionTreeRegressor', DecisionTreeRegressor()),
-#     ('RandomForestRegressor', RandomForestRegressor()),
-#     ('XGBRegressor', XGBRegressor()),
-#     ('KNeighborsRegressor', KNeighborsRegressor()),
-#     # ('PCA', PCA(n_components=4)),
-#     # ('SelectKBest', SelectKBest(score_func=f_classif, k="all")),
-#     ]
 
-def keep_features(data, features, keep_y=False):
+def keep_features(data, features: Union[List[str], List[int]]):
     # sort features highest to lowest
-    # if all(isinstance(x, int) for x in features):
-    features = sorted(features) #, reverse=True)
-
-    if isinstance(data, pd.DataFrame):
-        if keep_y:
-            return data.iloc[:, (features + [-1])]
-        else:
+    if all(isinstance(x, int) for x in features):
+        features = sorted(features)
+        if isinstance(data, pd.DataFrame):
             return data.iloc[:, features]
-    elif isinstance(data, np.ndarray):
-        if keep_y:
-            return data[:, (features + [-1])]
-        else:
-            return data[:, features]
-    elif isinstance(data, list):
-        if keep_y:
-            return [[row[i] for i in (features + [-1])] for row in data]
-        else:
+        elif isinstance(data, np.ndarray) or isinstance(data, list):
             return [[row[i] for i in features] for row in data]
     else:
-        raise ValueError("Input data type not supported. Use pandas DataFrame or numpy array.")
+        return data[features]
+
 
 def compare_features(results, names):
     # Compare Features
     plt.boxplot(results, labels=names)
-    plt.title('Algorithm Comparison')
+    plt.title("Algorithm Comparison")
     plt.show()
--- tests/unit/utils/test_utils_features_evaluation.py
+++ tests/unit/utils/test_utils_features_evaluation.py
@@ -3,40 +3,51 @@ import pandas as pd
 
 from libmlops.utils.features_evaluation import keep_features
 
-# Test cases using pytest
-def test_keep_features_pandas():
-    # Create a pandas DataFrame
-    df = pd.DataFrame({
-        'Feature1': [1, 2, 3],
-        'Feature2': [4, 5, 6],
-        'Target': [7, 8, 9]
-    })
-    
-    # Test keeping features and target column
-    result_df = keep_features(df, features=[1, 0], keep_y=True)
-    assert result_df.equals(df[['Feature1', 'Feature2', 'Target']])
 
-    # Test keeping only specific features
-    result_df = keep_features(df, features=[0], keep_y=False)
-    assert result_df.equals(df[['Feature1']])
+# Test cases using pytest
+def test_keep_features_dataframe():
+    # Create a pandas DataFrame
+    df = pd.DataFrame.from_dict(
+        {"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9], "D": [10, 11, 12]}
+    )
+
+    # Test keeping specific features
+    result_df = keep_features(df, features=["B", "A"])
+    expected_result = pd.DataFrame({"B": [4, 5, 6], "A": [1, 2, 3]})
+    pd.testing.assert_frame_equal(result_df, expected_result)
+
+    # Test keeping specific features with column indices
+    result_df = keep_features(df, features=[2, 0])
+    expected_result = pd.DataFrame({"A": [1, 2, 3], "C": [7, 8, 9]})
+    pd.testing.assert_frame_equal(result_df, expected_result)
+
+    # Test keeping all features
+    result_df = keep_features(df, features=list(df.columns))
+    pd.testing.assert_frame_equal(result_df, df)
 
 
 def test_keep_features_numpy():
     # Create a numpy array
     np_array = np.array([[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9, 12]])
-    
+
     # Test keeping features and target column
-    result_np = keep_features(np_array, features=[2, 0, 1], keep_y=True)
-    expected_result = np.array([[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9, 12]])
+    result_np = keep_features(np_array, features=[3, 0, 1])
+    expected_result = np.array([[1, 4, 10], [2, 5, 11], [3, 6, 12]])
     assert np.array_equal(result_np, expected_result)
 
-    # Test keeping only specific features
-    result_np = keep_features(np_array, features=[0, 2], keep_y=False)
-    expected_result = np.array([[1, 7], [2, 8], [3, 9]])
-    assert np.array_equal(result_np, expected_result)
+
+def test_keep_features_list():
+    # Create a list of lists
+    list_of_lists = [[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9, 12]]
+
+    # Test keeping features and target column
+    result_list = keep_features(list_of_lists, features=[2, 3, 0, 1])
+    expected_result = [[1, 4, 7, 10], [2, 5, 8, 11], [3, 6, 9, 12]]
+    assert result_list == expected_result
 
 
 # Run the tests
 if __name__ == "__main__":
     import pytest
+
     pytest.main([__file__])
