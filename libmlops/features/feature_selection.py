4e0deb0fd7e8a0158d4d6e986e68470e9bc46920
wolffy-au <wolffborg1@gmail.com>
2024-01-10 15:06:33 +1100

    Added convert_indices function

--- libmlops/features/feature_selection.py
+++ libmlops/features/feature_selection.py
@@ -1,12 +1,34 @@
 # Example feature_selection.py
 
-from sklearn.feature_selection import SelectKBest, f_classif, RFE, SelectFromModel, VarianceThreshold, SequentialFeatureSelector
+from typing import List
+from sklearn.feature_selection import (
+    SelectKBest,
+    f_classif,
+    RFE,
+    SelectFromModel,
+    VarianceThreshold,
+    SequentialFeatureSelector,
+)
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 
+
 # Feature Selection Functions: This file typically contains functions for selecting a subset of relevant features. This can help improve model performance and reduce overfitting.
 def select_k_best_features(X, Y, score_func=f_classif, k=10, verbose=False):
+    """
+    Selects the top k best features using a given scoring function.
+
+    Parameters:
+    - X (array-like): Feature matrix.
+    - Y (array-like): Target variable.
+    - score_func (callable, optional): Scoring function for feature selection. Default is f_classif.
+    - k (int, optional): Number of top features to select. Default is 10.
+    - verbose (bool, optional): If True, print additional information. Default is False.
+
+    Returns:
+    - X_selected (array-like): Feature matrix with selected features.
+    """
     selector = SelectKBest(score_func=score_func, k=k)
     X_selected = selector.fit_transform(X, Y)
     if verbose:
@@ -15,12 +37,43 @@ def select_k_best_features(X, Y, score_func=f_classif, k=10, verbose=False):
 
     return X_selected
 
+
 # Recursive Feature Elimination (RFE): Functions for recursively removing the least important features based on model performance.
 def select_recursive_feature_elimination(X, Y, n_features_to_select=10, verbose=False):
-    selector = RFE(LogisticRegression(solver='lbfgs', max_iter=1000), n_features_to_select=n_features_to_select)
+    """
+    Performs Recursive Feature Elimination (RFE) to recursively remove the least important features.
+
+    Parameters:
+    - X (array-like): Feature matrix.
+    - Y (array-like): Target variable.
+    - n_features_to_select (int, optional): Number of features to select. Default is 10.
+    - verbose (bool, optional): If True, print additional information. Default is False.
+
+    Returns:
+    - X_selected (array-like): Feature matrix with selected features.
+    """
+    selector = RFE(
+        LogisticRegression(solver="lbfgs", max_iter=1000),
+        n_features_to_select=n_features_to_select,
+    )
     X_selected = selector.fit_transform(X, Y)
     if verbose:
         # summarize scores
         print("X_selected:", X_selected)
 
     return X_selected
+
+
+# Convert indices  to feature names
+def convert_indices(dataset, features: List[int]):
+    """
+    Converts feature indices to corresponding feature names.
+
+    Parameters:
+    - dataset (DataFrame): The dataset with named columns.
+    - features (List[int]): List of feature indices.
+
+    Returns:
+    - feature_names (List[str]): List of corresponding feature names.
+    """
+    return [dataset.columns[v] for v in sorted(features)]
--- tests/unit/features/test_feature_selection.py
+++ tests/unit/features/test_feature_selection.py
@@ -1,10 +1,13 @@
 # test_feature_selection.py
 
 import numpy as np
+import pandas as pd
 from libmlops.features.feature_selection import (
     select_k_best_features,
     select_recursive_feature_elimination,
+    convert_indices,
 )
+
 
 def test_select_k_best_features():
     # Create dummy data for testing
@@ -18,10 +21,13 @@ def test_select_k_best_features():
     assert X_selected.shape == (X.shape[0], 10)  # Assuming default k=10
 
     # Test with custom parameters
-    X_selected_custom = select_k_best_features(X, Y, score_func=lambda X, Y: np.ones(X.shape[1]), k=5)
+    X_selected_custom = select_k_best_features(
+        X, Y, score_func=lambda X, Y: np.ones(X.shape[1]), k=5
+    )
 
     # Check that the shape of the selected features is correct for custom parameters
     assert X_selected_custom.shape == (X.shape[0], 5)
+
 
 def test_select_recursive_feature_elimination():
     # Create dummy data for testing
@@ -32,10 +38,30 @@ def test_select_recursive_feature_elimination():
     X_selected = select_recursive_feature_elimination(X, Y)
 
     # Check that the shape of the selected features is correct
-    assert X_selected.shape == (X.shape[0], 10)  # Assuming default n_features_to_select=10
+    assert X_selected.shape == (
+        X.shape[0],
+        10,
+    )  # Assuming default n_features_to_select=10
 
     # Test with custom parameters
-    X_selected_custom = select_recursive_feature_elimination(X, Y, n_features_to_select=5)
+    X_selected_custom = select_recursive_feature_elimination(
+        X, Y, n_features_to_select=5
+    )
 
     # Check that the shape of the selected features is correct for custom parameters
     assert X_selected_custom.shape == (X.shape[0], 5)
+
+
+# Test convert_indices function
+def test_convert_indices():
+    # Mock dataset
+    mock_data = {"feature_0": [1, 2, 3], "feature_1": [4, 5, 6], "feature_2": [7, 8, 9]}
+    mock_dataset = pd.DataFrame(mock_data)
+
+    feature_indices = [0, 2]
+    feature_names = convert_indices(mock_dataset, feature_indices)
+
+    # Assertions
+    expected_feature_names = ["feature_0", "feature_2"]
+    assert set(feature_names) == set(expected_feature_names)
+    # Add more assertions based on the expected behavior of the function
